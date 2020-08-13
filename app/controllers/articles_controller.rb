14856585dfd0823156effbefd4b1e3856e1c62f8
soundwebvertising <soundwebvertising@gmail.com>
2020-08-13 12:41:37 -0700

    refactoring excersise

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def show
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
   
   def index 
@@ -12,12 +14,13 @@ class ArticlesController < ApplicationController
   end
   
   def edit
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
   end
 
   def create 
     #render plain: params[:article]
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    #@article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       flash[:notice] = "Article was create successfully"
       redirect_to article_path(@article)
@@ -27,8 +30,9 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    #@article = Article.find(params[:id])
+   # if @article.update(params.require(:article).permit(:title, :description))
+   if @article.update(article_params)
       flash[:notice] = "Article was updated"
       redirect_to @article
     else
@@ -37,9 +41,18 @@ class ArticlesController < ApplicationController
   end
   
   def destroy
-    @article = Article.find(params[:id])
+    #@article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
   
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
