25a249753fa1b474f1ebfc8fd7c09009736f20e9
soundwebvertising <soundwebvertising@gmail.com>
2020-08-20 12:32:16 -0700

    Add edit and update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Your account was successfully updated"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   def create 
     @user = User.new(user_params)
     if @user.save
-      flash[:notice] = "Wecome to Alpha Blog #{@user.username}, you have successfully sign up"
+      flash[:notice] = "Welcome to Alpha Blog #{@user.username}, you have successfully sign up"
       redirect_to articles_path
     else
       render 'new'
